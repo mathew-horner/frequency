ca236540f76e800697228ced7d481958b2e38ed0
Mathew Horner <mathewhorner456@gmail.com>
2022-07-11 00:42:21 -0500

    Added a compact view for the habits

--- components/HabitCard.tsx
+++ components/HabitCard.tsx
@@ -12,9 +12,10 @@ import { HabitStatus } from "@prisma/client";
 
 interface Props {
   habit: TodayHabit;
+  compact?: boolean;
 }
 
-export default function HabitCard({ habit }: Props) {
+export default function HabitCard({ habit, compact }: Props) {
   const habitStatus = habit.today?.status || HabitStatus.Pending;
   const isPending = habitStatus === HabitStatus.Pending;
 
@@ -28,65 +29,125 @@ export default function HabitCard({ habit }: Props) {
     return null;
   }
 
-  return (
-    <Card
-      display="flex"
-      alignItems="center"
-      p={4}
-      gap={4}
-      h={24}
-      opacity={!isPending ? "0.5" : "1"}
-      position="relative"
-      overflow="hidden"
-    >
-      <Text
-        as="h1"
+  function renderCompact() {
+    return (
+      <Card
         display="flex"
         alignItems="center"
-        fontSize="2xl"
-        fontWeight="bold"
-        flexGrow={1}
-        gap={0.5}
+        h={12}
+        opacity={!isPending ? "0.5" : "1"}
+        position="relative"
+        overflow="hidden"
+        p={0}
       >
-        <Box>
-          {habit.title}
-          <Text fontSize="xs" textColor="gray.500">
-            due today
-          </Text>
-        </Box>
-      </Text>
-      {/* TODO: Streaks */}
-      <Flex alignItems="center" marginRight={4} textColor="gray.500">
+        <Text
+          as="h1"
+          display="flex"
+          alignItems="center"
+          fontSize="lg"
+          fontWeight="bold"
+          flexGrow={1}
+          p={4}
+        >
+          <Flex gap={2} alignItems="center">
+            {habit.title}
+            <Text fontSize="xs" textColor="gray.500">
+              (today)
+            </Text>
+          </Flex>
+        </Text>
+        {isPending ? (
+          <>
+            {/* Mark Incomplete Button */}
+            <Button h={16} w={16} p={0}>
+              <IoCloseCircleOutline size={32} />
+            </Button>
+
+            {/* Mark Completed Button */}
+            <Button
+              backgroundColor="black"
+              textColor="white"
+              _hover={{
+                backgroundColor: "gray.700",
+              }}
+              h={16}
+              w={16}
+              p={0}
+            >
+              <IoCheckmarkCircleOutline size={32} />
+            </Button>
+          </>
+        ) : (
+          <Flex alignItems="center" p={6}>
+            {renderCompletionIcon()}
+          </Flex>
+        )}
+      </Card>
+    );
+  }
+
+  function renderStandard() {
+    return (
+      <Card
+        display="flex"
+        alignItems="center"
+        p={4}
+        gap={4}
+        h={24}
+        opacity={!isPending ? "0.5" : "1"}
+        position="relative"
+        overflow="hidden"
+      >
+        <Text
+          as="h1"
+          display="flex"
+          alignItems="center"
+          fontSize="2xl"
+          fontWeight="bold"
+          flexGrow={1}
+          gap={0.5}
+        >
+          <Box>
+            {habit.title}
+            <Text fontSize="xs" textColor="gray.500">
+              due today
+            </Text>
+          </Box>
+        </Text>
+        {/* TODO: Streaks */}
+        {/* <Flex alignItems="center" marginRight={4} textColor="gray.500">
         <IoFlameSharp size={24} />
         <Text>6</Text>
-      </Flex>
-      {isPending ? (
-        <>
-          {/* Mark Incomplete Button */}
-          <Button h={16} w={16} p={0}>
-            <IoCloseCircleOutline size={32} />
-          </Button>
+      </Flex> */}
+        {isPending ? (
+          <>
+            {/* Mark Incomplete Button */}
+            <Button h={16} w={16} p={0}>
+              <IoCloseCircleOutline size={32} />
+            </Button>
 
-          {/* Mark Completed Button */}
-          <Button
-            backgroundColor="black"
-            textColor="white"
-            _hover={{
-              backgroundColor: "gray.700",
-            }}
-            h={16}
-            w={16}
-            p={0}
-          >
-            <IoCheckmarkCircleOutline size={32} />
-          </Button>
-        </>
-      ) : (
-        // Show how many points the user got for this habit.
-        <Flex alignItems="center" p={6}>
-          {renderCompletionIcon()}
-        </Flex>
-      )}
-    </Card>
-  );
+            {/* Mark Completed Button */}
+            <Button
+              backgroundColor="black"
+              textColor="white"
+              _hover={{
+                backgroundColor: "gray.700",
+              }}
+              h={16}
+              w={16}
+              p={0}
+            >
+              <IoCheckmarkCircleOutline size={32} />
+            </Button>
+          </>
+        ) : (
+          <Flex alignItems="center" p={6}>
+            {renderCompletionIcon()}
+          </Flex>
+        )}
+      </Card>
+    );
+  }
+
+  return compact ? renderCompact() : renderStandard();
 }
