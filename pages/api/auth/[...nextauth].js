2f3e2e58399545b1aad25d538815e8a6ca3c821f
Mathew Horner <mathewhorner456@gmail.com>
2022-07-17 10:05:58 -0500

    Add prisma adapter to create tables for users.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
+import { prisma } from "../../../utils/db";
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
 });
