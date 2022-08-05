22dcc32c54862de044db11c7df787789c90c7691
Mathew Horner <mathewhorner456@gmail.com>
2022-08-05 11:25:00 -0500

    Fix up landing page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import { useRef } from "react";
 
 import Button from "../components/Button";
+import Card from "../components/Card";
 import Footer from "../components/Footer";
 // import TestimonialCard from "../components/TestimonialCard";
 
@@ -58,7 +59,12 @@ export default function LandingPage() {
         </Flex>
 
         {/* Hero Section */}
-        <Flex justifyContent="center" flexGrow={1} backgroundColor="black" w="full"> 
+        <Flex
+          justifyContent="center"
+          flexGrow={1}
+          backgroundColor="black"
+          w="full"
+        >
           <Flex
             flexDir="column"
             gap={8}
@@ -74,7 +80,8 @@ export default function LandingPage() {
               Build habits the right way.
             </Text>
             <Text as="p" fontSize="xl" textColor="gray.300">
-              Life is busy, and chaotic. Your habit tracker should reflect that.<br/>
+              Life is busy, and chaotic. Your habit tracker should reflect that.
+              <br />
               With frequency, you will build the habits you need to live the
               life you want, with a method that works with even the most hectic
               of schedules.
@@ -100,46 +107,62 @@ export default function LandingPage() {
 
       {/* Info Section */}
       <Flex
-        flexDir="column"
-        gap={{ base: 8, lg: 16 }}
-        p={{ base: 8, lg: 16 }}
+        flexDir={{ base: "column", lg: "row" }}
+        gap={{ base: 8, xl: 16 }}
+        p={{ base: 8, xl: 16 }}
         ref={infoSectionRef}
+        maxW="1400px"
+        alignSelf="center"
       >
-        <Flex flexDir="column" gap={8}>
-          <Text fontSize="4xl" fontWeight="bold">
-            Habit trackers are getting it wrong.
-          </Text>
-          <Text fontSize="lg">
-            Most habit trackers force you into rigid schedules that don&apos;t
-            work with real lives. Scheduling habits for specific days introduces
-            too much rigidity into your life, and trying to do a little bit of
-            each habit every day doesn&apos;t scale.
-          </Text>
-        </Flex>
-        <Flex flexDir="column" gap={8}>
-          <Text fontSize="4xl" fontWeight="bold">
-            How frequency is different.
-          </Text>
-          <Text fontSize="lg">
-            frequency takes a new approach to building habits that we call the{" "}
-            <b>frequency method</b>. Here&apos;s how it works:
-          </Text>
-          <Text fontSize="lg">
-            Each habit has a <b>frequency</b>, which is set by you, the user.
-            This number represents how many days you can take to complete the
-            habit one time. Once you complete the habit, starting on the
-            following day, you have the same number of days to complete the
-            habit again. Repeat ad infinitum. You are able to complete habits
-            ahead of schedule if you&apos;d like, and habits will continue to
-            show up on your feed if they are past due until you complete them.
-          </Text>
-          <Text fontSize="lg">
-            The key features of this method are its scalability and flexibility.
-            By not having to cram every habit into every day, you can build more
-            habits in total. By being able to complete habits ahead or behind
-            schedule, you can plan ahead for habits you&apos;re likely to miss,
-            or make up for habits that you already missed.
-          </Text>
+        {/* Textual Content */}
+        <Box p={8} maxW="700px" alignSelf="center">
+          <Flex flexDir="column" gap={8}>
+            <Text fontSize="4xl" fontWeight="bold">
+              Habit trackers are getting it wrong.
+            </Text>
+            <Text fontSize="lg">
+              Most habit trackers force you into rigid schedules that don&apos;t
+              work with real lives. Scheduling habits for specific days
+              introduces too much rigidity into your life, and trying to do a
+              little bit of each habit every day doesn&apos;t scale.
+            </Text>
+          </Flex>
+          <Flex mt={8} flexDir="column" gap={8}>
+            <Text fontSize="4xl" fontWeight="bold">
+              How frequency is different.
+            </Text>
+            <Text fontSize="lg">
+              frequency takes a new approach to building habits that we call the{" "}
+              <b>frequency method</b>. Here&apos;s how it works:
+            </Text>
+            <Text fontSize="lg">
+              Each habit has a <b>frequency</b>, which is set by you, the user.
+              This number represents how many days you can take to complete the
+              habit one time. Once you complete the habit, starting on the
+              following day, you have the same number of days to complete the
+              habit again. Repeat ad infinitum. You are able to complete habits
+              ahead of schedule if you&apos;d like, and habits will continue to
+              show up on your feed if they are past due until you complete them.
+            </Text>
+            <Text fontSize="lg">
+              The key features of this method are its scalability and
+              flexibility. By not having to cram every habit into every day, you
+              can build more habits in total. By being able to complete habits
+              ahead or behind schedule, you can plan ahead for habits
+              you&apos;re likely to miss, or make up for habits that you already
+              missed.
+            </Text>
+          </Flex>
+        </Box>
+
+        {/* Images */}
+        <Flex flexDir={{ base: "column", md: "row", lg: "column" }} gap={8} alignItems="center">
+          <Card p={0} minW="400px" maxW="600px">
+            <img src="/app.png" />
+          </Card>
+          <Card p={0} minW="400px" maxW="600px">
+            <img src="/create.png" />
+          </Card>
         </Flex>
       </Flex>
 
